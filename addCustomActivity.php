e02f46a081ed5ed66c753cd71fa1a88d366925de
TimBluesWin <timisutantyo@gmail.com>
2019-06-23 21:40:37 +0700

    Halfway there to upload activity.
    This commit makes the user able to upload the image. The image is checked in terms of contents and extension, whether the uploaded image is actually an image file or not. Once the image is okay, the filename is then hashed using SHA256 based on the current time. Now, the only task remaining for uploading the image is to actually upload the image to the server, and store the activity details into the database.

---
+++ addCustomActivity.php
@@ -0,0 +1,55 @@
+<?php
+
+        $servername = "localhost";
+        $username = "root";
+        $password = "";
+        $dbname = "rett-comm";
+
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        if ($conn->connect_error)
+        {
+            die("Connection error: " . $conn->connect_error);
+        }
+        else
+        {
+            echo "success";
+        }
+
+        $activity = $_POST['activityName'];
+        #specify target directory
+        $target_dir = "/images";
+        #specify the path name of the file. The name of the image comes from the 'activity_image'
+        #from the html; we just need the name. Then, the obtained name is concatenated with the dir.
+        $target_file = $target_dir . basename($_FILES["activityImage"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        if(isset($_POST["submit"]))
+        {
+            $check = getimagesize($_FILES["activityImage"]["tmp_name"]);
+            if($check !== false)
+            {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadOk = 1;
+            }
+            else
+            {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif")
+        {
+            echo "Sorry, only JPG, JPEG, PNG, and GIF files are allowed.";
+            $uploadOk = 0;
+        }
+        if($uploadOk == 0)
+        {
+            echo "Sorry, your file was not uploaded.";
+        }
+        else
+        {
+            $hashed_file = hash("sha256", time().mt_rand(10,1000)).".".$imageFileType;
+            echo "The file is an image. Its hash is ".$hashed_file;
+        }
+
