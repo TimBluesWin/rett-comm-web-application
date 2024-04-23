a17209067beccd544005f9838aa4aad56a503808
Timmy <timisutantyo@gmail.com>
2024-04-23 18:40:53 +0200

    Fix non-working JQuery and Responsive Voice.js; I now use CDN links

--- mainMenu3.php
+++ mainMenu3.php
@@ -79,8 +79,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-<script src="jquery-3.4.1.min.js"></script>
-<script src="responsiveVoice.js"></script>
+<script
+  src="https://code.jquery.com/jquery-3.7.1.js"
+  integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4="
+  crossorigin="anonymous"></script>
+<script src="https://code.responsivevoice.org/responsivevoice.js?key=Ih6YLiFZ"></script>
 <?php
 include_once("config.php");
 ?>
