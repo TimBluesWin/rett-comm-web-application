40a781e609af435e02acb46b43c9804557f560d2
Timmy <timisutantyo@gmail.com>
2024-05-07 21:40:34 +0200

    Update JQuery and remove unneccessary JQuery and ResponsiveVoice files

--- indonesian/forCaregiver.php
+++ indonesian/forCaregiver.php
@@ -9,7 +9,10 @@ include('changeLanguage.php');
         <title> Untuk pengasuh </title>
     </head>
     <body>
-    <script src="jquery-3.4.1.min.js"></script>
+    <script
+    src="https://code.jquery.com/jquery-3.7.1.min.js"
+    integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo="
+    crossorigin="anonymous"></script>
     <?php changeLanguageDropDown(); ?>
     <br>
     <a href="addCustomActivity.html">Tambah aktivitas</a>
